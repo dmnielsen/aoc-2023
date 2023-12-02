a4b20d0f3803e0b49aaaca0b92c4a60c8b3edcd7
dmnielsen <dmnielsen@gmail.com>
2023-12-01 22:40:30 -0600

    Add day 1

---
+++ aoc2023/day_01.py
@@ -0,0 +1,64 @@
+import re
+from pathlib import Path
+
+from aoc2023 import AOC_DIR
+from aoc2023.util import print_solutions
+
+INPUT_FILENAME = AOC_DIR / 'inputs' / '202301_input.txt'
+
+INSTRUCTION_PATTERN = re.compile(r'(?=(\d|one|two|three|four|five|six|seven|eight|nine))')
+
+CONVERT_NUMBERS = {
+    word: i for i, word in enumerate(['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'], start=1)
+}
+
+
+def load(filename: Path = INPUT_FILENAME) -> str:
+    with open(filename, 'r') as f:
+        return f.read()
+
+
+def parse_input(text: str) -> list[str]:
+    return text.split('\n')
+
+
+def extract_instructions(text: str, p: re.Pattern = INSTRUCTION_PATTERN) -> int:
+    match = p.findall(text)
+    if match:
+        first = CONVERT_NUMBERS.get(match[0], match[0])
+        last = CONVERT_NUMBERS.get(match[-1], match[-1])
+
+        return int(f"{first}{last}")
+    else:
+        print(text)
+        return 0
+
+
+def solve_part1(input_: str) -> int:
+    instructions = parse_input(input_)
+
+    pattern = re.compile(r'\d')
+
+    return sum(extract_instructions(line, pattern) for line in instructions)
+
+
+def solve_part2(input_: str) -> int:
+    instructions = parse_input(input_)
+
+    calibration_values = [extract_instructions(line) for line in instructions]
+
+    return sum(calibration_values)
+
+
+def main(show_solution: bool = True):
+    input_ = load(INPUT_FILENAME)
+
+    result1 = solve_part1(input_)
+    result2 = solve_part2(input_)
+
+    if show_solution:
+        print_solutions(result1, result2)
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_day_01.py
@@ -0,0 +1,69 @@
+import pytest
+
+from aoc2023 import day_01 as day
+
+
+@pytest.fixture
+def mock_input():
+    return """1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet"""
+
+
+@pytest.fixture
+def mock_input_part2():
+    return """two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen"""
+
+
+def test_parse_input(mock_input):
+    expected = [
+        '1abc2',
+        'pqr3stu8vwx',
+        'a1b2c3d4e5f',
+        'treb7uchet',
+    ]
+
+    result = day.parse_input(mock_input)
+
+    assert all(a == b for a, b in zip(expected, result))
+
+
+@pytest.mark.parametrize(
+    'line,value',
+    [
+        ('1abc2', 12),
+        ('pqr3stu8vwx', 38),
+        ('a1b2c3d4e5f', 15),
+        ('treb7uchet', 77),
+        ('two1nine', 29),
+        ('eightwothree', 83),
+        ('abcone2threexyz', 13),
+        ('xtwone3four', 24),
+        ('4nineeightseven2', 42),
+        ('zoneight234', 14),
+        ('7pqrstsixteen', 76),
+        ('3asfdtwone', 31),
+    ],
+)
+def test_extract_instructions(line, value):
+    result = day.extract_instructions(line)
+    assert value == result
+
+
+def test_solve_part1(mock_input):
+    expected = 142
+    result = day.solve_part1(mock_input)
+    assert expected == result
+
+
+def test_solve_part2(mock_input_part2):
+    expected = 281
+    result = day.solve_part2(mock_input_part2)
+    assert expected == result
