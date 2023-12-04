411cf5929bba5c0d93d85c9364f19cec8289c1ff
dmnielsen <dmnielsen@gmail.com>
2023-12-04 00:20:38 -0600

    rename printing utility file

--- aoc2023/util/__init__.py
+++ aoc2023/util/__init__.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from print_solutions import print_solutions  # noqa: F401
+from .print_formatting import print_solutions  # noqa: F401
 
 NEW_DAY_TEMPLATE = Path(__file__).parent.absolute() / "day_template.txt"
 NEW_TEST_TEMPLATE = Path(__file__).parent.absolute() / "test_template.txt"
